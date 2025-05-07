fc0f42cc49700595713379d3adeb395680cb3ac3
Wagnerzachef <wagnerz@student.ncmich.edu>
2025-05-07 17:39:44 -0400

    Added data annotations to creat user request

--- Models/Requests/UserCreateRequest.cs
+++ Models/Requests/UserCreateRequest.cs
@@ -1,6 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BeverageAPI.Models.Requests {
     public class UserCreateRequest {
+        [Required]
+        [MaxLength(255)]
+        [MinLength(1)]
         public string Name {get; set;}
+        [Required]
         public DateTime dob {get; set;}
     }
 }
